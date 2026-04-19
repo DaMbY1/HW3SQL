f92cb167dad63729707aae6bc58276c937cfdb6d
DaMbY1 <dmitrybab80@mail.ru>
2026-04-20 02:32:49 +0300

    Update problem3.sql

--- problem3.sql
+++ problem3.sql
@@ -3,8 +3,7 @@ SELECT
     length(items) AS items_count,
     arraySum(prices) AS total_price,
     hasAll(items, ['laptop']) AS has_laptop,
-    arrayAvg(prices) AS avg_item_price
+    round(arrayAvg(prices), 2) AS avg_item_price
 FROM carts
-GROUP BY
-    cart_id, items, prices
-ORDER BY cart_id;
+GROUP BY cart_id
+ORDER BY total_price DESC;
